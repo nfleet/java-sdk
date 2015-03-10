fe63c13dfdb90d0cf6edc913e92b73df7f2e8e24
Marko Malinen <markojm1986@gmail.com>
2015-03-10 18:19:37 +0200

    Add getters and setters for DepotError properties

--- fi/cosky/sdk/DepotError.java
+++ fi/cosky/sdk/DepotError.java
@@ -6,4 +6,20 @@ import java.util.List;
 public class DepotError {
     private DepotData Depot;
     private List<ErrorData> Errors;
+
+    public DepotData getDepot() {
+        return Depot;
+    }
+
+    public void setDepot(DepotData depot) {
+        Depot = depot;
+    }
+
+    public List<ErrorData> getErrors() {
+        return Errors;
+    }
+
+    public void setErrors(List<ErrorData> errors) {
+        Errors = errors;
+    }
 }
