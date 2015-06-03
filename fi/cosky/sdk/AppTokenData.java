eded8abb9fbd656b4cdb7796d795c64ef4a65149
Jarkko Laitinen <jarkko.p.laitinen@jyu.fi>
2015-06-03 14:31:32 +0300

    Added missing file

---
+++ fi/cosky/sdk/AppTokenData.java
@@ -0,0 +1,14 @@
+package fi.cosky.sdk;
+
+public class AppTokenData extends BaseData {
+	
+	private String Token;
+
+	public String getToken() {
+		return Token;
+	}
+
+	public void setToken(String token) {
+		Token = token;
+	}
+}
